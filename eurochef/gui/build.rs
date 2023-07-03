a90e6285318b07ada1da1c0daead06ec95422a2b
cohaereo <cohaereo@protonmail.com>
2023-07-03 09:43:19 +0200

    gui: Add fallback for commit hash detection

--- eurochef/gui/build.rs
+++ eurochef/gui/build.rs
@@ -1,11 +1,12 @@
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
-        .output()
-        .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
+    let git_hash = if let Ok(output) = Command::new("git").args(&["rev-parse", "HEAD"]).output() {
+        String::from_utf8(output.stdout).unwrap()
+    } else {
+        "unknown".to_string()
+    };
+
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 
     let date_time = chrono::Utc::now();
